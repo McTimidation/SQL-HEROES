9f9911c8a2dd35aa7c22624c19668113e986fad7
McTimidation <michael.mcgraw@student.awesomeinc.org>
2022-11-03 17:46:33 +0000

    make_connection functional

--- src/database/delete_hero.py
+++ src/database/delete_hero.py
@@ -2,7 +2,7 @@ from connection import execute_query
 
 def delete_hero():
     remove_name = input('Enter name of Hero to Delete: ')
-    query = """DELETE FROM HEROES
+    query = """DELETE FROM heroes
             WHERE name = %s"""
     execute_query(query, (remove_name,))
     print("'"+remove_name+"'"+' deleted from existence')
